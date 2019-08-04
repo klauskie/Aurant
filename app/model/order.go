f216767942f3fc7e49621391ffd0ddc87ac4bc09
Klaus Kientzle <klaus.kientzle@gammapartners.com>
2019-08-04 14:28:41 -0600

    return item info in the order and errors fixed

--- app/model/order.go
+++ app/model/order.go
@@ -16,15 +16,17 @@ type Cart struct {
 	ID       int     `json:"cart_id"`
 	RestID 	 int     `json:"rest_id"`
 	ItemID 	 int     `json:"item_id"`
-	ClientID int     `json:"email"`
-	State    int     `json:"state_id"`
+	ClientID string  `json:"email"`
 	Date     string  `json:"datetime"`
+	State    int     `json:"state_id"`
 	AdInfo   string  `json:"additional_info"`
+	FullItem Item 	 `json:"full_item"`
 }
 
 var timeLayout string = "2006-01-02 15:04:05"
 
 const (
+	ZERO		= iota
 	NEW         = iota
 	ON_PROGRESS = iota
 	DELIVERED   = iota
@@ -44,14 +46,14 @@ func (cart *Cart) GetData() ([]byte, error) {
 	return output, err2
 }
 
-// GetDataByRestIDAndState : call getAllItemsByRestID
+// GetDataByRestIDAndState : call getAllOrdersByRestIDAndState
 func (order *Cart) GetDataByRestIDAndState(rest_id string, state string) ([]byte, error) {
 
 	usableID, _ := strconv.Atoi(rest_id)
 
 	data, err := getAllOrdersByRestIDAndState(usableID, state)
 	if err != nil {
-		log.Fatal("getAllItems error: ", err)
+		log.Fatal("getAllOrdersByRestIDAndState error: ", err)
 	}
 	output, err2 := json.Marshal(data)
 	if err2 != nil {
@@ -60,14 +62,14 @@ func (order *Cart) GetDataByRestIDAndState(rest_id string, state string) ([]byte
 	return output, err2
 }
 
-// GetDataByClientAndRestID : call
+// GetDataByClientAndRestID : call getAllOrdersByClientAndRestID
 func (order *Cart) GetDataByClientAndRestID(email string, restID string) ([]byte, error) {
 
 	usableID, _ := strconv.Atoi(restID)
 
 	data, err := getAllOrdersByClientAndRestID(email, usableID)
 	if err != nil {
-		log.Fatal("getAllItems error: ", err)
+		log.Fatal("getAllOrdersByClientAndRestID error: ", err)
 	}
 	output, err2 := json.Marshal(data)
 	if err2 != nil {
@@ -160,7 +162,12 @@ func scanRowsOrder(rows *sql.Rows) ([]*Cart, error) {
 	for rows.Next() {
 		var cart Cart
 
-		err := rows.Scan(&cart.ID, &cart.RestID, &cart.ItemID, &cart.ClientID, &cart.State, &cart.Date, &cart.AdInfo)
+		err := rows.Scan(&cart.ID, &cart.RestID, &cart.ItemID, &cart.ClientID, &cart.Date, &cart.State, &cart.AdInfo)
+		if err != nil {
+			return m, err
+		}
+
+		err = implantFullItem(&cart)
 		if err != nil {
 			return m, err
 		}
@@ -171,9 +178,23 @@ func scanRowsOrder(rows *sql.Rows) ([]*Cart, error) {
 	return m, nil
 }
 
+// implantFullItem : find the item and add it to the cart
+func implantFullItem(cart *Cart) error {
+
+	row := config.DB.QueryRow("SELECT * FROM ITEM WHERE item_id = ?", cart.ItemID)
+	err := row.Scan(&cart.FullItem.ID, &cart.FullItem.RestID, &cart.FullItem.CategoryID, &cart.FullItem.Name,
+		&cart.FullItem.Description, &cart.FullItem.Price, &cart.FullItem.IsEnabled)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func (order *Cart) insertIntoDB() error {
-	_, err := config.DB.Exec("INSERT INTO CART(rest_id, item_id, email, state_id, datetime, additional_info) VALUES (?,?,?,?,?,?)",
-		order.RestID, order.ItemID, order.ClientID, NEW, time.Now().Format(timeLayout), order.AdInfo)
+	_, err := config.DB.Exec("INSERT INTO CART(rest_id, item_id, email, datetime, state_id, additional_info) VALUES (?,?,?,?,?,?)",
+		order.RestID, order.ItemID, order.ClientID, time.Now().Format(timeLayout), NEW, order.AdInfo)
 	if err != nil {
 		return err
 	}
@@ -181,7 +202,7 @@ func (order *Cart) insertIntoDB() error {
 }
 
 func (order *Cart) updateState() error {
-	_, err := config.DB.Exec("UPDATE CART SET state_id = ?, date = ? WHERE cart_id = ?", order.State, time.Now().Format(timeLayout), order.ID)
+	_, err := config.DB.Exec("UPDATE CART SET state_id = ?, datetime = ? WHERE cart_id = ?", order.State, time.Now().Format(timeLayout), order.ID)
 	if err != nil {
 		return err
 	}
@@ -203,7 +224,7 @@ func incrementStateByOne (cartID int) (map[string]string, error) {
 
 	if actualState <= CLOSED {
 		actualState += 1
-		_, err := config.DB.Exec("UPDATE CART SET state_id = ?, date = ? WHERE cart_id = ?", actualState, time.Now().Format(timeLayout), cartID)
+		_, err := config.DB.Exec("UPDATE CART SET state_id = ?, datetime = ? WHERE cart_id = ?", actualState, time.Now().Format(timeLayout), cartID)
 		if err != nil {
 			message["state"] = ""
 			return message, err
