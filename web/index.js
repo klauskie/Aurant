a75f877ba698a579609c554927404de7232eb51b
Klaus Kientzle <klaus.kientzle@gammapartners.com>
2019-08-25 10:58:47 -0600

    Admin dev started. No docker

--- web/index.js
+++ web/index.js
@@ -1,12 +1,31 @@
 const express = require('express')
 var request = require('request')
+const engines = require('consolidate');
 
 const app = express()
 const port = 4000
 
+app.engine('ejs', engines.ejs);
+app.set('views', './views');
+app.set('views engine', 'ejs');
+
+
+app.get('/', function(req, res){
+  
+})
+
 app.get('/item', (req, res) => {
-  console.log('hola baka')
-  res.send('Welcome to Make REST API Calls In Express!')
+  console.log('/item')
+  request('http://localhost:8080/item', function (error, response, body) {
+    if (error != null) {
+      res.send("error")
+    }
+    if (response.statusCode != 200) {
+      res.send("not 200")
+    }
+    var info = JSON.parse(body);
+    res.render('home.ejs', {info:info})
+  });
 })
   
 
@@ -15,3 +34,4 @@ app.listen(port, function () {
 });
 
 
+// npx nodemon index.js
